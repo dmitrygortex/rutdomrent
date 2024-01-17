0644a3d09ae7e2a098e00bbc27aaebea0f82d095
rachhhen <skeletonpanda@mail.ru>
2024-01-17 15:45:06 +0300

    Change text on main activity

---
+++ app/src/main/java/com/example/rut_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rut_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
