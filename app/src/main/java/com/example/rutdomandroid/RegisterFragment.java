42d7ee64704f1b7d16054df387bee9bfddf063df
dmitry.go <dmitry.go@inbox.ru>
2024-01-26 04:35:30 +0500

    Add binding and edit XML register

--- app/src/main/java/com/example/rutdomandroid/RegisterFragment.java
+++ app/src/main/java/com/example/rutdomandroid/RegisterFragment.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.rutdomandroid.databinding.FragmentLoginBinding;
@@ -16,13 +18,29 @@ import com.example.rutdomandroid.databinding.FragmentRegisterBinding;
 public class RegisterFragment extends Fragment {
     FragmentRegisterBinding binding;
     TextView loginLabel;
+    EditText editTextLogin;
+    EditText editTextFullName;
+    EditText editTextEmail;
+    EditText editTextPassword;
+    EditText editTextInstitute;
+    EditText editTextPhoneNumber;
+    Button buttonRegistration;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentRegisterBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        loginLabel=binding.loginLabel;
+        loginLabel = binding.loginLabel;
+        editTextLogin = binding.editTextLogin;
+        editTextFullName = binding.editTextFullName;
+        editTextInstitute = binding.editTextInstitute;
+        editTextPassword = binding.editTextPassword;
+        editTextPhoneNumber = binding.editTextPhoneNumber;
+        buttonRegistration = binding.button;
+
         loginLabel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +52,13 @@ public class RegisterFragment extends Fragment {
                         .commit();
             }
         });
+        buttonRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         // Inflate the layout for this fragment
         return view;
     }
