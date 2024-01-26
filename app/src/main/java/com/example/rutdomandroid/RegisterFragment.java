722825c44bab4a83e4c9267f6e5b601a7bb9ab6a
George <lunohod0002@gmail.com>
2024-01-26 23:42:07 +0300

    Edt rent fragment

--- app/src/main/java/com/example/rutdomandroid/RegisterFragment.java
+++ app/src/main/java/com/example/rutdomandroid/RegisterFragment.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -33,16 +32,10 @@ import java.util.Map;
 
 public class RegisterFragment extends Fragment {
     FragmentRegisterBinding binding;
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
 
-    TextView loginLabel;
-    EditText editTextLogin;
-    EditText editTextFullName;
-    EditText editTextEmail;
-    EditText editTextPassword;
-    EditText editTextInstitute;
-    EditText editTextPhoneNumber;
-    Button buttonRegistration;
-
+    Button loginButton;
 
 
     @Override
@@ -50,33 +43,86 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentRegisterBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-        loginLabel = binding.loginLabel;
-        editTextLogin = binding.editTextLogin;
-        editTextFullName = binding.editTextFullName;
-        editTextInstitute = binding.editTextInstitute;
-        editTextPassword = binding.editTextPassword;
-        editTextPhoneNumber = binding.editTextPhoneNumber;
-        buttonRegistration = binding.button;
+        loginButton = binding.button;
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        loginLabel.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager fragmentManager =requireActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, new LoginFragment(), null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack("name") // Name can be null
-                        .commit();
-            }
-        });
-        buttonRegistration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                String email = binding.emailText.getText().toString();
+                String password = binding.passwordText.getText().toString();
+                String name = binding.FIOText.getText().toString();
+                String institute = binding.institute.getText().toString();
+                if (email.isEmpty()) {
+                    Toast.makeText(getContext(), "Поле email  должно быть заполнено", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 8) {
+                    Toast.makeText(getContext(), "Минимальная длина пароля 8 символов", Toast.LENGTH_SHORT).show();
 
-            }
-        });
+                    return;
+                }
+                if (password.isEmpty()) {
+                    Toast.makeText(getContext(), "Поле Пароль должно быть заполнено", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (institute.isEmpty()) {
+                    Toast.makeText(getContext(), "Поле Институт должно быть заполнено", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (name.isEmpty()) {
+                    Toast.makeText(getContext(), "Поле ФИО должно быть заполнено", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    String uid = user.getUid();
 
-        // Inflate the layout for this fragment
+                                    Map<String, Object> userMap = new HashMap<>();
+                                    userMap.put("email", email);
+                                    userMap.put("password", password);
+                                    userMap.put("ФИО", name);
+                                    userMap.put("Институт", institute);
+
+                                    userMap.put("uid", uid);
+
+                                    db.collection("users").document(uid)
+                                            .set(userMap)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void unused) {
+                                                    Toast.makeText(inflater.getContext(), "Аккаунт создан.",
+                                                            Toast.LENGTH_SHORT).show();
+                                                    FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                                                    fragmentManager.beginTransaction()
+                                                            .replace(R.id.frame_layout, new InfoFragment(), null)
+                                                            .setReorderingAllowed(true)
+                                                            .addToBackStack("name")
+                                                            .commit();
+                                                }
+
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getContext(), "Попробуйте еще раз",
+                                                            Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(getContext(), "Попробуйте еще раз.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+
+        });
         return view;
     }
 }
--- app/src/main/java/com/example/rutdomandroid/TimeFragment.java
+++ app/src/main/java/com/example/rutdomandroid/TimeFragment.java
@@ -2,6 +2,7 @@ package com.example.rutdomandroid;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,17 +10,31 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.rutdomandroid.databinding.FragmentLoginBinding;
 import com.example.rutdomandroid.databinding.RentTimeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TimeFragment extends Fragment {
     RentTimeBinding binding;
+    Bundle bundle;
+    FirebaseAuth auth ;
+    FirebaseFirestore db;
+    String data,room,issue,time,date;
+
     List<String> time_values;
     RecyclerView recyclerView;
     TimeAdapter timeAdapter;
@@ -29,6 +44,15 @@ public class TimeFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = RentTimeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        Bundle bundle;
+        bundle = getArguments();
+         auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        if (bundle != null) {
+            data = bundle.getString("date");
+        }
+        Toast.makeText(getContext(), data, Toast.LENGTH_SHORT).show();
+
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(inflater.getContext(), RecyclerView.VERTICAL, false);
         List<String> timeValues = Arrays.asList(getResources().getStringArray(R.array.time_values));
 
@@ -41,6 +65,51 @@ public class TimeFragment extends Fragment {
         recyclerView = binding.timeRecycler;
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(timeAdapter);
+        binding.bookingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<TimeSlot> timeValues=timeAdapter.getTimeSlots();
+                for (TimeSlot timeSlot : timeValues) {
+
+                    if (timeSlot.isSelected()) {
+                        time = timeSlot.getTime();
+                        break;
+                    }
+                }
+                if (time.isEmpty()){
+                    Toast.makeText(getContext(), "Выберите время мероприятия.", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                String[] values = data.split("_");
+                date = values[0];
+                room = values[1];
+                issue = values[2];
+                Map<String, Object> map = new HashMap<>();
+                Map<String, Object> data = new HashMap<>();
+                data.put("0", auth.getCurrentUser().getUid());
+                data.put("1", room);
+                data.put("2", issue);
+                map.put(time, data);
+                db.collection("booking").
+                        document(date).
+                        set(map, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getContext(), "Помещение забронировано.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "Попробуйте еще раз.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+
+            }
+        });
         return view;
     }
 
